9dcafed8e38581832bf9607a6ce1fa663cf0bb83
Margarito <jimenez8382@gmail.com>
2017-01-26 17:50:30 -0600

    Adding Unity

---
+++ Services/ExpensesServices.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace Services
+{
+    public interface IExpensesServices
+    {
+        List<Expenses> GetExpenses();
+    }
+    public class ExpensesServices : IExpensesServices
+    {
+        private readonly ExpensesDbContext _db;
+        public TotalExpensesServices(ExpensesDbContext db)
+        {
+            this._db = db;
+        }
+
+
+        public List<Expenses> GetExpenses()
+        {
+            return _db.Expens.ToList();
+        }
+        
+    }
+}
