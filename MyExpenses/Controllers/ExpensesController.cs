ec4a80971ba7d30cb2934e5c6c8f59db7af6e028
Margarito <jimenez8382@gmail.com>
2017-01-26 19:16:07 -0600

    adding action and feature to Edit and delete record

--- MyExpenses/Controllers/ExpensesController.cs
+++ MyExpenses/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using Services;
 using Services.Models.Expenses;
+using System;
 using System.Web.Mvc;
 
 namespace MyExpenses.Controllers
@@ -16,26 +17,80 @@ namespace MyExpenses.Controllers
             return View(_service.GetExpenses());
 
         }
-        [HttpPost]
-        public ActionResult Create(ExpenseItem model)
-        {
-            this.TempData["Notification"] = "Your record was created successfully.";
-            this.TempData["NotificationCSS"] = "notificationbox nb-success";
-
-            ////To display an error use
-            //this.TempData["NotificationCSS"] = "notificationbox nb-error";
-
-            ////To display a warning use
-            //this.TempData["NotificationCSS"] = "notificationbox nb-warning";
-
-            var itemAdded = _service.AddItem(model);
-            return RedirectToAction("Index");
-        }
 
         [HttpGet]
         public ActionResult Create()
         {
             return View(new ExpenseItem());
+        }
+        [HttpPost]
+        public ActionResult Create(ExpenseItem model)
+        {
+          
+            ////To display an error use
+            //this.TempData["NotificationCSS"] = "notificationbox nb-error";
+
+            ////To display a warning use
+            //this.TempData["NotificationCSS"] = "notificationbox nb-warning";
+            try
+            {
+                var itemAdded = _service.AddItem(model);
+                this.TempData["Notification"] = "Your record was created successfully.";
+                this.TempData["NotificationClass"] = "notificationbox notibox-success";
+            }
+            catch (Exception ex)
+            {
+                this.TempData["Notification"] = "We had a problem to save your Record,Verify your information and try again.";
+                this.TempData["NotificationClass"] = "notificationbox notibox-error";
+            }
+            return RedirectToAction("Index");
+        }
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Edit(long Id)
+        {
+            // find the row we need to update and return new ExpenseItem
+            var item = _service.GetItem(Id);
+            var editIt = new ExpenseItem
+            {
+                Expenses = item.Amount,
+                Date = item.Date,
+                Decription = item.Description,
+                ExpensesId = item.Id
+            };
+            return View(editIt);
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(ExpenseItem model)
+        {
+            try
+            {
+                var item = _service.EditItem(model);
+                this.TempData["Notification"] = "Your record was updated successfully.";
+                this.TempData["NotificationClass"] = "notificationbox notibox-success";
+            }
+            catch (Exception ex)
+            {
+                this.TempData["Notification"] = "We had a problem to Update your Record,Verify your information and try again.";
+                this.TempData["NotificationClass"] = "notificationbox notibox-error";
+            }
+            return RedirectToAction("Index");
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Delete(long Id)
+        {
+            try
+            {
+                var item = _service.DeleteItem(Id);
+                this.TempData["Notification"] = "Your record was deleted successfully.";
+                this.TempData["NotificationClass"] = "notificationbox notibox-success";
+            }
+            catch (Exception ex)
+            {
+                this.TempData["Notification"] = "We had a problem to delete your Record,try again.";
+                this.TempData["NotificationClass"] = "notificationbox notibox-error";
+            }
+            return RedirectToAction("Index");
         }
     }
 }
